fe1b37deac214f9f5588d1f852c9423ac84c1e3c
fb0801 <fbhatti0801@gmail.com>
2023-06-01 18:54:18 +0100

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # shopping-cart
 Shopping cart built using REACTJS and Typescript
+
+original project repo -> https://github.com/weibenfalk/react-shopping-cart
+
+# Resources used:
+
+https://www.youtube.com/watch?v=sfmL6bGbiN8&pp=ygUTcmVhY3Qgc2hvcHBpbmcgY2FydA%3D%3D 
+
+https://flatlogic.com/blog/material-ui-icons-in-react/
+
+https://www.smashingmagazine.com/2020/07/styled-components-react/
+
+https://www.toptal.com/developers/gitignore/
---
+++ react-shopping-cart/src/App.tsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import { useQuery } from 'react-query';
+
+//components
+import  Drawer  from '@material-ui/core/Drawer';
+import  LinearProgress  from '@material-ui/core/LinearProgress';
+import  Grid  from '@material-ui/core/Grid';
+import  AddShoppingCart  from '@material-ui/icons/AddShoppingCart';
+
+function App() {
+  return (
+    <div className="App">
+     start
+    </div>
+  );
+}
+
+export default App;
